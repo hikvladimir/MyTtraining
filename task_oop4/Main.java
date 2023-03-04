adebff128ea8d3c60076e24d772e7078159f6d09
hikvladimir <psihouse@mail.ru>
2023-03-04 21:27:41 +0300

    task5.1

--- task_oop4/Main.java
+++ task_oop4/Main.java
@@ -2,8 +2,8 @@ package task_oop4;
 
 public class Main {
     public static void main(String[] args) {
-        Worker employee = new Worker("Иван", "Программист", 50000);
-        System.out.println(employee.getInfo(12));
+        Worker Worker = new Worker("Иван", "Программист", 50000);
+        System.out.println(Worker.getInfo(10));
     }
 
 }
