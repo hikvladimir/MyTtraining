187221287b9a32032d5de246d27839c2bd9ffff8
hikvladimir <psihouse@mail.ru>
2023-03-09 21:12:05 +0300

    task6

---
+++ task_oop5/Main.java
@@ -0,0 +1,12 @@
+package task_oop5;
+
+public class Main {
+    public static void main(String[] args) {
+        Monster monster1 = new Monster();
+        Monster monster2 = new Monster(1,1,1);
+        Monster monster3 = new Monster(4,2);
+        monster1.voice();
+        monster2.voice(3);
+        monster3.voice(5, "Я монстр 3");
+    }
+}
