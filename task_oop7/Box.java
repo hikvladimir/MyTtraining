fdfd92e3eaea04f26878477915721ceeff646300
hikvladimir <psihouse@mail.ru>
2023-04-03 18:30:46 +0300

    task7

---
+++ task_oop7/Box.java
@@ -0,0 +1,48 @@
+package task_oop7;
+
+public class Box {
+    double height;
+    double width;
+    double length;
+    Box(double height, double width, double length) {
+        this.height=height;
+        this.width=width;
+        this.length=length;
+    }
+    Box(Box box1, Box box2) {
+        this.width = box1.width + box2.width;
+        this.length = box1.length + box2.length;
+        this.height = box1.height + box2.height;
+    }
+    Box() {
+        width = 0;
+        height = 0;
+        length = 0;
+    }
+
+    Box newBox(Box box){
+        return new Box(this.width + box.width, this.height + box.height, this.length + box.length);
+    }
+    double volume() {
+        return width * height * length;
+    }
+
+    void showVolume() {
+        System.out.println(volume());
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+
+}
+
---
+++ task_oop7/Main.java
@@ -0,0 +1,15 @@
+package task_oop7;
+
+
+
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Box box1 = new Box(10.00, 10.00, 10.00);
+
+        System.out.println(box1.getHeight());
+    }
+}
