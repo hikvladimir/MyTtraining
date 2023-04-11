410c446440fc1bc10fc53e087a5cd4db9fde2d46
hikvladimir <psihouse@mail.ru>
2023-04-11 18:37:49 +0300

    task13.0

---
+++ task14/Main.java
@@ -0,0 +1,10 @@
+package task14;
+
+public class Main {
+    public static void main(String[] args) {
+
+            int random = (int) (Math.random());
+            String result = String.format("Случайное число . Попробуйте еще раз...", random);
+            System.out.println(result);
+    }
+}
