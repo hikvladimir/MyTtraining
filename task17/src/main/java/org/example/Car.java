c4042290d2a7286e7e041c05201682aad74903de
hikvladimir <psihouse@mail.ru>
2023-04-20 17:47:35 +0300

    task16

---
+++ task17/src/main/java/org/example/Car.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Car {
+    private String brend;
+    private int number;
+
+    public Car(String brend, int number) {
+        this.brend = brend;
+        this.number = number;
+    }
+
+    public String getBrend() {
+        return brend;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}
---
+++ task17/src/test/java/org/example/CarListTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import static org.junit.Assert.*;
+
+public class CarListTest {
+
+    private CarList carList;
+    @org.junit.Before
+    public void setUp() throws Exception {
+        //init
+    }
+}
